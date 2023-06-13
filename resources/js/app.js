3ce412247ea9d67971b1a8a6aa1b45e3c26c37f8
tei0110 <tei01100110@gmail.com>
2023-06-13 22:27:04 +0900

    installed vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+// import Alpine from 'alpinejs';
+
+// window.Alpine = Alpine;
+
+// Alpine.start();
+
+import { createApp } from 'vue';
+import PostsIndex from './components/Posts/Index.vue';
+
+createApp({})
+    .component('PostsIndex', PostsIndex)
+    .mount('#app')
